329764b0efda239166d81c8d8b8b64ec204d19a4
Jamal Kayed <119996570+Yuroq@users.noreply.github.com>
2023-11-18 17:01:32 -0500

    Add files via upload

--- client/src/Components/dashBoardNav.jsx
+++ client/src/Components/dashBoardNav.jsx
@@ -9,12 +9,12 @@ function DashboardNav() {
     <div className="drawer lg:drawer-open" style={{ display: "flex" }}>
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
-        <label
+        {/* <label
           htmlFor="my-drawer-2"
           className="btn btn-primary drawer- NavLink lg:hidden"
         >
           Open drawer
-        </label>
+        </label> */}
       </div>
       <div className="drawer-side" style={{ width: 250 }}>
         <label
